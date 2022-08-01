fd6ee43ef23290485a9b8be09375e8324454636c
mafiti91 <78339633+mafiti91@users.noreply.github.com>
2022-08-01 21:45:43 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 
 module.exports = function towelSort (matrix) {
     let arr = [];
+    if( matrix === undefined ) return [];
     for (let i = 0; i < matrix.length; i ++) {
         for (let j = 0; j < matrix[i].length; j ++) {
             if(i % 2 === 0) arr.push(matrix[i][j])
